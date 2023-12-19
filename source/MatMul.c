d3c54c2d8add343b0fc97977f1b31c783e946ddb
here_io <52244687+iopark@users.noreply.github.com>
2023-12-20 07:50:11 +0900

    feat: mat mul
    none

--- source/MatMul.c
+++ source/MatMul.c
@@ -1,273 +1,298 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> 
 #include <stdbool.h>
 #include <stdlib.h>
-void PrintResult(int *result, char *fileName, int row, int column);
-void SaveToHeap(FILE* input, int* address);
-bool Init(FILE *input, int **result, int *out_row, int *out_column);
-bool GetFormat(FILE* input, int *matInfo);
-bool Multiply(FILE* input, int* result, int* refRow, int* refColumn, int **doubleResult);
-bool AddFileName(char* val, char* fileName, char**initFile);
+#include <memory.h>
+
+typedef struct matrix
+{
+	char *name; 
+	int row; 
+	int column; 
+	double *flattenMatrixValue; 
+} matrix; 
+
+bool UpdateFilenameToOut(matrix newMatrix, matrix* outBuffer); 
+bool SaveFilenameToOut(char *fileName, matrix *outBuffer); 
+void PrintMatrixWithName(double *result, char *fileName, int row, int column); 
+bool ValidateMatrixDimension(FILE* input, int *outDimensionValue);
+bool MultiplyAndOverwrite(matrix *multiplicand, matrix multiplier); 
+bool SaveMatrixArrayToOut(FILE *input, int matrixDimInfo[], matrix *outMatrix); 
+
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+	if (argc < 2)
+	{
+		printf("Usage:/ Requires 2 arguments, input file"); 
+		return 1; 
+	}
+	FILE* input = NULL; 
+	int fileCount = argc -1;
+    matrix ouputBuffer; 
+    ouputBuffer.name = NULL; 
+    ouputBuffer.flattenMatrixValue = NULL; 
+
+    for (int count = 1; count <= argc -1; count++)
     {
-        printf("Usage:/ Requires 2 arguments, input file");
-        return 1;
-    }
-    //Required Variables
-    int* result = NULL;
-    FILE* input = NULL;
-    int r_Row = 0;
-    int r_Column = 0;
-    int inputText = argc;
-    char* fileName = NULL;
-    int count = 1;
-    //First File
-    input = fopen(argv[1], "r");
-    if (input == NULL)
-    {
-        printf("File on Invalid Path");
-        return 1;
-    }
-    if (Init(input, &result, &r_Row, &r_Column))
-    {
-        if (!AddFileName(argv[1], fileName, &fileName))
-        {
-            printf("Failed To Init FileName");
-            return 1;
-        }
-        fclose(input);
-    }
-    else
-    {
-        printf("Couldn't initialize a file");
-        return 1;
-    }
-    count++;
-    while (count <= argc -1)
-    {
+        matrix newMatrixBuffer; 
         input = fopen(argv[count], "r");
-        if (!Multiply(input, result, &r_Row, &r_Column, &result))
+		if (input == NULL)
+		{
+			printf("File Not Found in Current Directory %s", argv[count]); 
+			return 1; 
+		}
+        if (!SaveFilenameToOut(argv[count], &newMatrixBuffer))
         {
-            printf("Multiiplication logic error occured");
-            return 1;
+            printf(" %s Text file name insert failed", argv[count]); 
+            return 1; 
         }
-        if (!AddFileName(argv[count], fileName, &fileName))
+		int matrixDimValue[2] = {0,}; 
+		if (!ValidateMatrixDimension(input, &matrixDimValue))
+		{
+			printf("Matrix Value And or Dimension Not Valid LINE 61"); 
+			return 1; 
+		}
+        if (!SaveMatrixArrayToOut(input, matrixDimValue, &newMatrixBuffer))
         {
-            printf("Filename Appending went wrong, 60");
-            return 1;
+            printf("Matrix value failed to be saved"); 
+            return 1; 
         }
-        count++;
-        //free(input);
-        fclose(input);
-    }
-    // Create an multidimensional array
-    // Underlying assumptions
-    // There will be no error resulting from
-    // Attempt 1: Simply brute force my way into this code.
-    // ATtempt 2: Strassen algorithm -> Divide and conquer until base case 2x2;
-    if (result != NULL)
-        PrintResult(result, fileName, r_Row, r_Column);
-    else
-    {
-        printf("Input is somehow null");
-        return 1;
-    }
-    free(result);
-    free(input);
-    fclose(input);
-}
-void SaveToHeap(FILE* input, int* address)
-{
-    int val;
-    int count = 0;
-    fseek(input, 0L, SEEK_SET);
-    while (fscanf(input, "%d", &val) != EOF)
-    {
-        if (count< 2)
+        if (!MultiplyAndOverwrite(&ouputBuffer, newMatrixBuffer))
         {
-            count++;
-            continue;
+            printf("Multiplication logic error occured"); 
+            return 1; 
         }
-        address[count-2] = val;
-        count++;
+        if (!UpdateFilenameToOut(newMatrixBuffer, &ouputBuffer))
+        {
+            printf("Filename Appending went wrong, 60"); 
+            return 1; 
+        } 
+        PrintMatrixWithName(newMatrixBuffer.flattenMatrixValue, newMatrixBuffer.name, newMatrixBuffer.row, newMatrixBuffer.column);
+        count++; 
+        free(newMatrixBuffer.flattenMatrixValue); 
+        free(newMatrixBuffer.name); 
+        fclose(input); 
     }
+	PrintMatrixWithName(ouputBuffer.flattenMatrixValue, ouputBuffer.name, ouputBuffer.row, ouputBuffer.column);
+	free(ouputBuffer.flattenMatrixValue); 
+    free(ouputBuffer.name); 
 }
-bool Init(FILE* input, int **result, int *out_row, int *out_column)
-{
-    int matInfo[2] = { 0, };
-    GetFormat(input, &matInfo[0]);
-    if (out_row == NULL || out_column == NULL) return false;
-    *out_row = matInfo[0];
-    *out_column = matInfo[1];
-    //NULL Exceptions prior to malloc
-    int *temp = malloc((*out_row) * (*out_column) * sizeof(int));
-    if (temp == NULL)
-    {
-        return false;
-    }
-    SaveToHeap(input, temp);
-    *result = temp;
-    return true;
-}
+
 /// <summary>
-/// Retrives Y and X value from Matrix
+/// Retrives Y and X value from Matrix, also contest whether there's appropriate mount of data
 /// </summary>
 /// <param name="input"></param>
 /// <param name="matInfo"></param>
 /// <returns></returns>
-bool GetFormat(FILE* input, int *matInfo)
+bool ValidateMatrixDimension(FILE* input, int *outDimensionValue)
 {
-    int val;
-    int count = 0;
-    fseek(input, 0L, SEEK_SET);
-    while (fscanf(input, "%d", &val) != EOF)
-    {
-        if (count < 2)
+	int evaluatedValue; 
+	int count = 0; 
+
+    fseek(input, 0L, SEEK_SET); 
+	while (fscanf(input, "%d", &evaluatedValue) != EOF)
+	{
+		if (count < 2)
+		{
+			outDimensionValue[count] = evaluatedValue;
+			count++;
+			continue; 
+		}
+		else
+		{
+			count++; 
+		}
+	}
+	int total = outDimensionValue[0] * outDimensionValue[1]; 
+	if (total == (count-2))
+	{
+		return true; 
+	}
+	else
+	{
+		printf("Invalid number of input"); 
+		return false; 
+	}
+}
+
+bool SaveMatrixArrayToOut(FILE *input, int matrixDimInfo[2], matrix *outMatrix)
+{
+	int size = matrixDimInfo[0] + matrixDimInfo[1]; 
+    printf("\n %d, %d\n", matrixDimInfo[0], matrixDimInfo[1]); 
+	double *matrixBuffer = malloc(sizeof(double) *size); 
+	if (matrixBuffer == NULL)
+	{
+		return false; 
+	}
+    memset(matrixBuffer, 0.0, sizeof(double)*size); 
+    fseek(input, 0L, SEEK_SET); 
+    double val;
+	int count = 0; 
+	while (fscanf(input, "%lf", &val) != EOF)
+	{
+        if (count< 2) 
         {
-            matInfo[count] = val;
-            count++;
-            continue;
-        }
-        else
-            break;
-    }
-    return true;
+            count++; 
+            continue; 
+        } 
+		matrixBuffer[count-2] = val;
+        count++; 
+	}
+    outMatrix->row = matrixDimInfo[0]; 
+    outMatrix->column = matrixDimInfo[1]; 
+    outMatrix->flattenMatrixValue = matrixBuffer; 
+	return true; 
 }
 /// <summary>
-/// Takes in previous matrix result * next matrix
+/// Takes in previous matrix result * next matrix 
 /// </summary>
 /// <param name="next">next is treated like next matrix value</param>
 /// <param name="result">result is first treated like prev</param>
 /// <param name="refRow"></param>
 /// <param name="refColumn"></param>
 /// <returns></returns>
-bool Multiply(FILE *next, int *result, int *refRow, int *refColumn, int **doubleResult)
+bool MultiplyAndOverwrite(matrix *multiplicand, matrix multiplier)
 {
-    if (result == NULL)
+    if (multiplicand->flattenMatrixValue == NULL)
     {
-        printf("result is Null");
+        int Y = multiplier.row; 
+		int X = multiplier.column;  
+		multiplicand->row = Y; 
+		multiplicand->column = X; 
+		multiplicand->flattenMatrixValue = multiplier.flattenMatrixValue; 
+        return true; 
     }
-    int matInfo[2] = { 0, };
-    GetFormat(next, &matInfo[0]);
-    int nextY = matInfo[0];
-    int nextX = matInfo[1];
-    if (refRow == NULL || refColumn == NULL) return false;
-    int* nextMat = (int*)malloc(sizeof(int) * (nextY * nextX));
-    if (nextMat == NULL)
-    {
-        printf("Line 145: Memory Shortage");
-        return false;
-    }
-    SaveToHeap(next, nextMat);
-    int newY = *refRow;
-    int newX = nextX;
-    int *destMat = malloc(sizeof(int) * (newY * newX));
-    if (destMat == NULL)
-    {
-        printf("Line 131: Memory Shortage");
-        return false;
-    }
-    // i is equivalent to prev's rows value
-    for (int i = 0; i < newY; i++)
-    {
-        // j is equivalent to next's column value
-        for (int j = 0; j < newX; j++)
-        {
-            int temp = 0;
-            //prev * next
-            for (int k = 0; k < nextY; k++)
-            {
-                int leftIndex = k + (i * nextY);
-                int rightIndex =  k*newX + j;
-                //printf("\nLINE 197: %d, %d, right Index: %d", result[leftIndex], nextMat[rightIndex], rightIndex);
-                temp += result[leftIndex] * nextMat[rightIndex];
-            }
-            int index = i*newX + j; // i *
-            destMat[index] = temp;
-        }
-    }
-    *refRow = newY;
-    *refColumn = newX;
-    int* dispose = result;
-    *doubleResult = destMat;
-    free(nextMat);
-    free(dispose);
-    return true;
+	int nextY = multiplier.row; 
+	int nextX = multiplier.column; 
+	int prevY = multiplicand->row;
+	int prevX = multiplicand->column; 
+
+	if (prevX != nextY)
+	{
+		printf("\n%d, %d \n", prevX, nextY); 
+		printf("Matrix Size needs to be in order, such that a x b * c x d, b == c"); 
+		return false; 
+	} 
+
+	int newY = prevY; 
+	int newX = nextX; 
+	double *destMat = malloc(sizeof(int) * (nextY * prevX));
+	if (destMat == NULL)
+	{
+		printf("Line 131: Memory Shortage"); 
+		return false;
+	}
+	for (int i = 0; i < newY; i++)
+	{
+		for (int j = 0; j < newX; j++)
+		{
+			double temp = 0;
+			for (int k = 0; k < nextY; k++)
+			{
+				int leftIndex = k + (i * nextY); 
+				int rightIndex =  k*newX + j; 
+				temp += multiplicand->flattenMatrixValue[leftIndex] * multiplier.flattenMatrixValue[rightIndex]; 
+			}
+			int index = i*newX + j; // i * 
+			destMat[index] = temp; 
+		}
+	}
+	multiplicand->row = newY; 
+	multiplicand->column = newX; 
+	double* dispose = multiplicand->flattenMatrixValue;
+	multiplicand->flattenMatrixValue = destMat;
+	free(dispose);
+	return true; 
 }
-/// <summary>
-/// Adjustable, based on the format name
-/// </summary>
-/// <param name="val"></param>
-/// <param name="fileName"></param>
-/// <param name="index"></param>
-/// <returns></returns>
-bool AddFileName(char* val, char* fileName, char**initFile)
+
+/// @brief Simply Takes in the parameter value to print its array, Clears up heap value ON the matrix struct 
+/// @param result Resulting address to the heap 
+/// @param fileName resulting filename 
+/// @param row in int 
+/// @param column in int 
+void PrintMatrixWithName(double *result, char *fileName, int row, int column)
 {
-    //If its the first file ever===========================================
-    if (fileName == NULL)
-    {
-        int length = strlen(val) - 3;
-        char* tempCon = malloc(sizeof(char) * length);
-        if (tempCon == NULL)
-        {
-            printf("Line 154, Memory shortage");
-            return false;
-        }
-        for (int i = 0; i < length - 1; i++)
-        {
-            tempCon[i] = val[i];
-        }
-        tempCon[length-1] = '\0';
-        *initFile = tempCon;
-        return true;
-    }
-    //else ===============================================================
-    int n = 0;
-    char *temp;
-    int wordLength = strlen(val);
-    int fileNameLength = strlen(fileName);
-    int formatLoc = wordLength - 4;
-    int newLength = fileNameLength + formatLoc +1;
-    temp = malloc(sizeof(char)*newLength);
-    if (temp == NULL)
-    {
-        printf("Line 174, Memory Shortage");
-        free(fileName);
-        return false;
-    }
-    strcpy(temp, fileName);
-    for (int i = fileNameLength; i < newLength; i++)
-    {
-        temp[i] = val[i - fileNameLength];
-    }
-    temp[newLength -1] = '\0';
-    if (fileName != NULL)
-    {
-        char *dispose = fileName;
-        free(dispose);
-    }
-    *initFile = temp;
-    return true;
-}
-void PrintResult(int *result, char *fileName, int row, int column)
-{
-    printf("%s = \n", fileName);
-    int count = 0;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            int index = j + (column * i);
-            printf("%d ", result[index]);
-        }
-        count++;
+	int index = 0; 
+    int titleIndex = strlen(fileName) -4;
+	while( fileName[index] != '\0')
+	{
+		printf("%c", fileName[index]);
+		index++;
+	} 
+	printf("=\n"); 
+    int count = 0; 
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			int index = j + (column * i); 
+			printf("%.2f ", result[index]); 
+		}
+        count++; 
         if (count < row)
         {
             printf(" \n");
         }
-    }
-    free(result);
-    free(fileName);
+	}
+	printf("\n");  
+}
+
+bool SaveFilenameToOut(char *fileName, matrix *outBuffer)
+{
+	int titleIndex = strlen(fileName) -3;
+	char *newName = malloc(sizeof(char) * titleIndex); 
+	if (newName == NULL)
+	{
+		printf("Memory Shortage"); 
+		return false; 
+	}
+    memset(newName, 0, sizeof(char)*titleIndex); 
+	for (int i = 0; i < titleIndex; i++)
+	{
+		newName[i] = fileName[i]; 
+	}
+	newName[titleIndex-1] = '\0';
+    outBuffer->name = newName; 
+    return true; 
+}
+
+bool UpdateFilenameToOut(matrix newMatrix, matrix* outBuffer)
+{
+	if (outBuffer->name == NULL)
+	{
+        int titleIndex = strlen(newMatrix.name);
+		char *copyName = malloc(sizeof(char) * titleIndex);
+		if (copyName == NULL)
+		{
+			return false; 
+		} 
+        memset(copyName, 0, sizeof(char)*titleIndex); 
+		strcpy(copyName, newMatrix.name); 
+		outBuffer->name = copyName; 
+		return true; 
+	}
+	int n = 0; 
+    char *temp; 
+	int wordLength = strlen(newMatrix.name); 
+	int accumulated = strlen(outBuffer->name); 
+	int formatLoc = wordLength - 4; 
+	int newLength = accumulated + formatLoc +1; 
+
+	temp = malloc(sizeof(char)*newLength); 
+	if (temp == NULL)
+	{
+		printf("Line 174, Memory Shortage"); 
+		return false; 
+	}
+    memset(temp, 0, sizeof(char)*newLength); 
+    strcpy(temp, outBuffer->name); 
+	for (int i = accumulated; i < newLength; i++)
+	{
+		temp[i] = newMatrix.name[i - accumulated]; 
+	}
+	temp[newLength -1] = '\0';
+	char *dispose = outBuffer->name; 
+	outBuffer->name = temp;
+	free(dispose); 
+	return true; 
 }
